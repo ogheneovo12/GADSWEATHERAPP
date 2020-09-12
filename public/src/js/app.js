8c48c6ed4b67129bd76e1a10989ab8e6ad4bf1a3
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-09-12 18:06:06 +0100

    resolved geo issue

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -9,36 +9,36 @@ const btn = document.querySelector("#btnget");
 initApp();
 
 btn.addEventListener("click", (e) => {
- e.preventDefault();
+  e.preventDefault();
   const query = `${input.value}`;
   if (!!!query) return UI.errorAlert("please enter a valid city");
   handleSearch(`q=${query}`);
 });
 
-
-function handleSearch(query){
-    UI.displayLoading("main");
-    getWeather(query)
-      .then(
-        (data) => {
-          UI.removeLoading();
-          UI.displayWeatherResult(data);
-          updateSearchesUi();
-        },
-        (error) => {
-          UI.removeLoading();
-          UI.errorAlert(error);
-        }
-      )
-      .catch(console.log);
+function handleSearch(query) {
+  UI.displayLoading("main");
+  getWeather(query)
+    .then(
+      (data) => {
+        UI.removeLoading();
+        UI.displayWeatherResult(data);
+        updateSearchesUi();
+      },
+      (error) => {
+        UI.removeLoading();
+        UI.errorAlert(error);
+      }
+    )
+    .catch(console.log);
 }
 function getWeather(query) {
-  return fetch(`${api_url}/?${query}&units=metric&appid=47eb3373f62109aa49c6d644e4cf2d9f`).then(handleResponse);
+  return fetch(
+    `${api_url}/?${query}&units=metric&appid=47eb3373f62109aa49c6d644e4cf2d9f`
+  ).then(handleResponse);
 
   function handleResponse(response) {
     return response.text().then((text) => {
       if (!response.ok) {
-        
         if (response.status === 401 || response.status === 500) {
           return Promise.reject(
             "soory we couldn't accesss our server, this is our fault, our engineers are on it:( "
@@ -50,7 +50,7 @@ function getWeather(query) {
       }
       const data = text && JSON.parse(text);
       relevantData = getRelevantData(data);
-     StorageHelper.SaveData(relevantData);
+      StorageHelper.SaveData(relevantData);
       return relevantData;
     });
   }
@@ -59,25 +59,29 @@ function getWeather(query) {
 function initApp() {
   UI.greet();
   UI.displayLoading();
-  getUserLocation().then(location=>{
-      console.log(location)
-      handleSearch(`lat=${location.latitude}&lon=${location.longitude}`)
-  }).catch(error=>{
-    UI.errorAlert(error.message)
-    const searches = StorageHelper.getData();
-    if (!searches || !searches.length) {
-      //get user geolocation, make request based on geolocation
+
+  //check if the user has made any searches before 
+  //if not use the user location for first search
+  const searches = StorageHelper.getData();
+  if (!searches || !searches.length) {
+    getUserLocation()
+    .then((location) => {
+      handleSearch(`lat=${location.latitude}&lon=${location.longitude}`);
+    })
+    .catch((error) => {
+      UI.errorAlert(error);
       UI.removeLoading();
-      return;
-    }
-    updateSearchesUi();
-    UI.displayWeatherResult(searches.pop());
-    UI.removeLoading();
-  })
-  
+    });
+    return;
+  }
+
+  updateSearchesUi();
+  UI.displayWeatherResult(searches.pop());
+  UI.removeLoading();
+
 }
 
-function updateSearchesUi(){
+function updateSearchesUi() {
   const searches = StorageHelper.getData();
   UI.displaySearchHistory(searches);
 }
