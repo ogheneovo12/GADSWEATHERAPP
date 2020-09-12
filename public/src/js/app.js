4e22977aeb03b07b87d962fd682391001be5e3ba
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-09-12 09:29:11 +0100

    removed env files

---
+++ public/src/js/app.js
@@ -0,0 +1,71 @@
+const api_url = "https://api.openweathermap.org/data/2.5/weather/";
+const input = document.querySelector("#inputui");
+const btn = document.querySelector("#btnget");
+
+//implement greeting
+
+//change update function
+//then submit
+initApp();
+
+btn.addEventListener("click", () => {
+  const query = `${input.value}`;
+  if (!!!query) return UI.errorAlert("please enter a valid city");
+  UI.displayLoading("main");
+  getWeather(query)
+    .then(
+      (data) => {
+        UI.removeLoading();
+        UI.displayWeatherResult(data);
+        updateSearchesUi();
+      },
+      (error) => {
+        UI.removeLoading();
+        UI.errorAlert(error);
+      }
+    )
+    .catch(console.log);
+});
+
+function getWeather(query) {
+  return fetch(`${api_url}/?q=${query}&units=metric&appid=`).then(handleResponse);
+
+  function handleResponse(response) {
+    return response.text().then((text) => {
+      if (!response.ok) {
+        
+        if (response.status === 401 || response.status === 500) {
+          return Promise.reject(
+            "soory we couldn't accesss our server, this is our fault, our engineers are on it:( "
+          );
+        }
+        const data = text && JSON.parse(text);
+        const error = (data && data.message) || response.statusText;
+        return Promise.reject(error);
+      }
+      const data = text && JSON.parse(text);
+      relevantData = getRelevantData(data);
+     StorageHelper.SaveData(relevantData);
+      return relevantData;
+    });
+  }
+}
+
+function initApp() {
+  UI.greet();
+  UI.displayLoading();
+  const searches = StorageHelper.getData();
+  if (!searches || !searches.length) {
+    //get user geolocation, make request based on geolocation
+    UI.removeLoading();
+    return;
+  }
+  updateSearchesUi();
+  UI.displayWeatherResult(searches.pop());
+  UI.removeLoading();
+}
+
+function updateSearchesUi(){
+  const searches = StorageHelper.getData();
+  UI.displaySearchHistory(searches);
+}
